02ad7b51297ebe3463a27a2d5f5c66716f888dc7
KwwClw <neskung50@gmail.com>
2023-12-14 10:30:33 +0700

    add new code

--- main.py
+++ main.py
@@ -2,16 +2,12 @@ import os
 import discord
 from dotenv import load_dotenv
 from discord.ext import commands
-
-intents = discord.Intents.default()
-intents.voice_states = True
-
 from keep_alive import keep_alive
-keep_alive()
-
-load_dotenv()
 
 bot = commands.Bot(command_prefix='k!', intents=discord.Intents.all())
+
+load_dotenv()
+TOKEN = os.environ.get('TOKEN')
 
 @bot.event
 async def on_ready():
@@ -23,13 +19,14 @@ async def on_voice_state_update(member, before, after):
 
     if notification_channel is not None and before.channel != after.channel:
         if after.channel is not None:
-            await notification_channel.send(f'{member.mention} has joined {after.channel.name}.')
+            await notification_channel.send(f'{member} has joined {after.channel.name}.')
         elif before.channel is not None:
-            await notification_channel.send(f'{member.mention} has left {before.channel.name}.')
+            await notification_channel.send(f'{member} has left {before.channel.name}.')
 
 # @bot.event
 # async def on_member_joined(member):
 #     chanel = bot.get_channel(1134149073662922753)
 
+keep_alive()
 
-bot.run(os.environ['TOKEN'])
+bot.run(TOKEN)
