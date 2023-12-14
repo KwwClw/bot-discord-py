2e9c1a463da06d3aa9804d4274cec06baa6ecf65
KwwClw <neskung50@gmail.com>
2023-12-14 15:31:37 +0700

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import os
 import discord
 from dotenv import load_dotenv
 from discord.ext import commands
+from discord import app_commands
 from keep_alive import keep_alive
 
 bot = commands.Bot(command_prefix='k!', intents=discord.Intents.all())
@@ -22,6 +23,20 @@ async def on_voice_state_update(member, before, after):
             await notification_channel.send(f'{member} has joined {after.channel.name}.')
         elif before.channel is not None:
             await notification_channel.send(f'{member} has left {before.channel.name}.')
+
+@bot.command()
+async def hello(ctx):
+    await ctx.send(f"Hello {ctx.author.name}!")
+
+@bot.tree.command(name = 'help', description = 'help command')
+async def helpcommand(interaction):
+    helpembed = discord.Embed(
+        title = 'Help',
+        description = 'Bot Commands',
+        color = 0x3559E0,
+        timestamp = discord.utils.utcnow()
+    )
+    await interaction.send_send(embed = helpembed)
 
 # @bot.event
 # async def on_member_joined(member):
